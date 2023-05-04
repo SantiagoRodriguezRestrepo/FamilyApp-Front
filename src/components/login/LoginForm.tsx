0b3433d4c26637407dc8693224f4a0ce7234cd1d
Juan Sebastian Paez <jpaez@loyaltysci.com>
2023-05-04 13:44:47 -0500

    feat: Agrega funcionalidad de ver contrasena en login

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Col, Form } from "react-bootstrap";
+import { Button, Col, Form, InputGroup } from "react-bootstrap";
 import { END_POINTS } from "../../constants/Api";
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { fetchWrapper, handleOnlyNumbers } from "../../utils/functions";
 import { toast } from "sonner";
 import LogoImage from '../../assets/logo64.png';
@@ -11,6 +12,7 @@ export const LoginForm = () => {
   const [userId, setUserId] = useState<string>('');
   const [userPassword, setUserPassword] = useState<string>('');
   const [searchQuery, setSearchQuery] = useState<string>('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -44,15 +46,15 @@ export const LoginForm = () => {
         <h1 className="h-auto">Familiapp</h1>
       </LogoContainer>
       <LoginContainer >
-        <RowLogin  className="row w-100 m-0">
+        <RowLogin className="row w-100 m-0">
           <Col className="d-flex justify-content-center align-items-center flex-column">
-            <SearchContainer  className="d-flex gap-4 w-50">
+            <SearchContainer className="d-flex gap-4 w-50">
               <Form.Control name="search" id="search" type="text" autoComplete="off" placeholder="Ingresar cédula" value={searchQuery} onChange={handleInputChange} />
               <ButtonCustom color="primary" onClick={handleSearch}> Buscar </ButtonCustom>
             </SearchContainer>
           </Col>
           <Col md="auto" className="d-flex align-items-center">
-            <Hr  className="mx-auto" />
+            <Hr className="mx-auto" />
           </Col>
           <Col className="d-flex justify-content-center align-items-center">
             <Form className="w-50" onSubmit={handleLogin}>
@@ -60,10 +62,21 @@ export const LoginForm = () => {
                 <Form.Label className="text-white">Usuario</Form.Label>
                 <Form.Control name="identification" id="identification" type="text" autoComplete="off" placeholder="Ingresar cédula" value={userId} onChange={handleInputChange} />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label className="text-white">Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Ingresar contraseña" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
-              </Form.Group>
+              <Form.Label className="text-white">Contraseña:</Form.Label>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  type={showPassword ? 'text' : 'password'}
+                  onChange={(e) => setUserPassword(e.target.value)}
+                />
+                <Button
+                  variant="secondary"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {
+                    showPassword ? <AiFillEyeInvisible /> : <AiFillEye />
+                  }
+                </Button>
+              </InputGroup>
               <ButtonCustom color="primary" className="float-right" type="submit"> Ingresar </ButtonCustom>
             </Form>
           </Col>
